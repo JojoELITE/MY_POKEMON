d2283c4a1a154f226a666fc81d8c3f2d57cb264e
JojoELITE <jojoelite1014@gmail.com>
2024-03-07 12:47:07 +0100

    My App Pokemon

--- src/components/Concerning.js
+++ src/components/Concerning.js
@@ -1,39 +1,39 @@
 import React from "react";
 import { useGetSpeciesQuery, useGetGenderQuery } from "../data/RecoverData";
 
-const About = ({ details, id }) => {
-  const species = useGetSpeciesQuery(`${id}`);
-  const gender = useGetGenderQuery(`${id}`);
+const Concerning = ({ pokemonDetails, pokemonId }) => {
+  const speciesQuery = useGetSpeciesQuery(`${pokemonId}`);
+  const genderQuery = useGetGenderQuery(`${pokemonId}`);
 
-  if (species.isLoading || gender.isLoading) {
+  if (speciesQuery.isLoading || genderQuery.isLoading) {
     return <p>Loading...</p>;
   }
-  if (species.error) {
-    return <p>Error: {species.error?.data || gender.error?.data}</p>;
+  if (speciesQuery.error) {
+    return <p>Error: {speciesQuery.error?.data || genderQuery.error?.data}</p>;
   }
 
-  const data = species.data || null;
-  const gender_data = gender.error ? { name: "Not found" } : gender.data;
+  const speciesData = speciesQuery.data || null;
+  const genderData = genderQuery.error ? { name: "Not found" } : genderQuery.data;
 
   return (
-    <div className="w-[47%]">
+    <div className="w-[47%] bg-gray-100 p-6 rounded-lg shadow-lg">
       <div className="">
         <p className="flex justify-between mb-3">
           <span className="text-slate-500">Species</span>
-          <span className="font-bold">{details.species.name}</span>
+          <span className="font-bold">{pokemonDetails.species.name}</span>
         </p>
         <p className="flex justify-between mb-3">
           <span className="text-slate-500">Height</span>
-          <span className="font-bold">{details.height}</span>
+          <span className="font-bold">{pokemonDetails.height}</span>
         </p>
         <p className="flex justify-between mb-3">
           <span className="text-slate-500">Weight</span>
-          <span className="font-bold">{details.weight}</span>
+          <span className="font-bold">{pokemonDetails.weight}</span>
         </p>
         <div className="flex justify-between mb-3">
           <span className="text-slate-500">Abilities</span>
           <p>
-            {details.abilities.map((el, index) => {
+            {pokemonDetails.abilities.map((el, index) => {
               return (
                 <span className="font-bold ml-2" key={index}>
                   {el.ability.name}
@@ -43,23 +43,23 @@ const About = ({ details, id }) => {
           </p>
         </div>
       </div>
-      <div className="w-full">
+      <div className="w-full mt-4 border-t pt-4">
         <h2 className="font-bold text-lg">Breeding</h2>
         <p className="flex justify-between mb-3">
           <span className="text-slate-500">Gender</span>
-          <span className="font-bold">{gender_data.name}</span>
+          <span className="font-bold">{genderData.name}</span>
         </p>
         <p className="flex justify-between mb-3">
           <span className="text-slate-500">Egg Groups</span>
-          <span className="font-bold">{data.egg_groups[0].name}</span>
+          <span className="font-bold">{speciesData.egg_groups[0].name}</span>
         </p>
         <p className="flex justify-between mb-3">
           <span className="text-slate-500">Egg Cycle</span>
-          <span className="font-bold">{details.species.name}</span>
+          <span className="font-bold">{pokemonDetails.species.name}</span>
         </p>
       </div>
     </div>
   );
 };
 
-export default About;
+export default Concerning;
